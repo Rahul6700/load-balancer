aa604e73981f26f9a0516375bd2d1514fb0a93e7
Rahul6700 <rahulsk4087@gmail.com>
2025-05-09 16:42:31 +0530

    feat: SelectServer func

---
+++ core/redirect.go
@@ -0,0 +1,32 @@
+package core
+
+import (
+	"fmt"
+	"github.com/Rahul6700/load-balancer/models"
+)
+
+func SelectServer () *models.ServerInput {
+
+	//var to store the chosen server
+	var ChosenServer models.ServerInput;
+	
+	ChosenServer = ServerArray[0] //chosing the first by default
+	
+	var temp int
+	temp = 0
+	
+	// picking the server with the lowest 'Active' value
+	for i, server := range ServerArray {
+		if(server.Active > ChosenServer.Active) {
+			ChosenServer = ServerArray[i]
+			temp = i
+		}
+	}
+
+	// incrementing the Active count for the chosen array
+	ServerArray[temp].Active++
+
+	return ChosenServer
+
+}
+
